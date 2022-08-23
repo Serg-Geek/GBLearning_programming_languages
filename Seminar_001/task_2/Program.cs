f647cf93f59a2d0146c25e2f9ad6be69c06c32b8
serg <svgordienko@yandex.ru>
2022-08-23 17:28:25 +0400

    add task 2

---
+++ Seminar_001/task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (max > b)
+{
+   Console.WriteLine($"max ={max}");
+}
+else
+{
+    Console.WriteLine($"max ={b}");
+}
