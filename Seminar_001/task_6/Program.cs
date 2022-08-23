d005b2401eabee986c3dd6bba069e84718b9efe4
serg <svgordienko@yandex.ru>
2022-08-23 23:06:39 +0400

    add task 6

---
+++ Seminar_001/task_6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num =  int.Parse(Console.ReadLine());
+
+if (num % 2==1)
+{
+   Console.WriteLine("No");
+}
+else
+{
+    Console.WriteLine("Yes");
+}
+
+ 
+
+
+
