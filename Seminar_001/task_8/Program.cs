fb4f9d369003c3404675997826b84d82e47b23c4
serg <svgordienko@yandex.ru>
2022-08-24 00:44:43 +0400

    add foolproof in task_8

--- Seminar_001/task_8/Program.cs
+++ Seminar_001/task_8/Program.cs
@@ -1,11 +1,8 @@
 ﻿Console.WriteLine("Введите число: ");
-
 int num =  int.Parse(Console.ReadLine());
-
+if (num<=1) Console.WriteLine ("Вы ввели неправильное число(должно быть больше 1)");
 int current = 1;
-
 while (current <= num)
-
 {
     if (current % 2 == 1)
         {
@@ -17,4 +14,3 @@ while (current <= num)
             current++;
         }
 }
-
