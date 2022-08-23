63efa1515d3688e23cb441c917a8cb30a2b71b81
serg <svgordienko@yandex.ru>
2022-08-24 00:29:57 +0400

    add Task 8

---
+++ Seminar_001/task_8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num =  int.Parse(Console.ReadLine());
+
+int current = 1;
+
+while (current <= num)
+
+{
+    if (current % 2 == 1)
+        {
+             current++;
+        }
+    else
+        {
+            Console.Write($"{current} ,");
+            current++;
+        }
+}
+
