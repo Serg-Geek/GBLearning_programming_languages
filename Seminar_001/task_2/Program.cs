122215d14bc596b96c2d37fb8d4b2a9815e30098
serg <svgordienko@yandex.ru>
2022-08-24 12:39:28 +0400

    add condition of the task.

--- Seminar_001/task_2/Program.cs
+++ Seminar_001/task_2/Program.cs
@@ -1,11 +1,16 @@
-﻿Console.WriteLine("Введите число а");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число а");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число b");
 int b = int.Parse(Console.ReadLine());
-int max = a;
-if (max > b)
+if (a > b)
 {
-   Console.WriteLine($"max ={max}");
+   Console.WriteLine($"max ={a}");
 }
 else
 {
--- Seminar_001/task_6/Program.cs
+++ Seminar_001/task_6/Program.cs
@@ -1,7 +1,10 @@
-﻿Console.WriteLine("Введите число: ");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
 
+Console.WriteLine("Введите число: ");
 int num =  int.Parse(Console.ReadLine());
-
 if (num % 2==1)
 {
    Console.WriteLine("No");
@@ -10,8 +13,3 @@ else
 {
     Console.WriteLine("Yes");
 }
-
- 
-
-
-
