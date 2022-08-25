a5ba0f4424c830261ce02637abcbeaec5714f368
serg <svgordienko@yandex.ru>
2022-08-26 03:04:15 +0400

    add Tasks seminar2

---
+++ Seminar_2/task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8 
+//918 -> 1
+Console.WriteLine("Виедите трехзначное число: ");
+int num =  int.Parse(Console.ReadLine());
+if (num >999 && num <100) Console.WriteLine(" число: ");
+else
+{
+    //int a2 = num % 100;
+    //int result = a2/10;
+    int result = (num%100)/10;
+    Console.WriteLine($"{result}");
+}
