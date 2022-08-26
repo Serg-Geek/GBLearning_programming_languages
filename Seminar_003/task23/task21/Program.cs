dcf333d28404fa60b7c043f221f1b5a77814d0a2
serg <svgordienko@yandex.ru>
2022-08-26 18:14:59 +0400

    add Seminar 003

---
+++ Seminar_003/task23/task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("input x1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("input y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("input z1: ");
+double  z = double.Parse(Console.ReadLine());
+
+Console.WriteLine("input x2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("input y2: ");
+double y2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("input z2: ");
+double z2 = double.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))+((z1-z2)*(z1-z2)));
+Console.WriteLine($"{result:f2}");// "{result:f2}" два знака после запятой
---
+++ Seminar_003/task23/task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+//14212 -> нет/
+//23432 -> да
+//12821 -> да
+Console.WriteLine("Введите пятизначное  число: ");
+int num =  int.Parse(Console.ReadLine());
+if (num <10000 || num > 99999)
+{
+  Console.WriteLine(" Вы ввели не пятизначное  число.");
+}
+int num1 = num/10000;
+int num2 = (num % 10000)/1000;
+int num4= (num % 100)/10;
+int num5= num% 10;
+
+if (num1==num5 && num2 == num4)
+{
+    Console.WriteLine("ДА");  
+}
+else
+{
+    Console.WriteLine("HET");
+}
