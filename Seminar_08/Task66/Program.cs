2900d8628d5698c116ae9c98967292118a1ff029
serg <svgordienko@yandex.ru>
2022-09-22 00:34:16 +0400

    add seminar08

---
+++ Seminar_08/Task66/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Фунция заполнения массива
+int[] CreateArray(int minValue, int maxValue)
+{
+    int size = maxValue - minValue - 1;
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = minValue + i + 1;
+    }
+    return array;
+}
+int NaturalisSum(int[] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            res += array[i];
+        }
+    }
+    return res;
+}
+
+// Функция получение строки и проверка на число.
+int Check()
+{
+    int number;
+    while (true)
+    {
+        Console.WriteLine($"Введите число: ");
+        string str = Console.ReadLine();
+        try
+        {
+            number = Convert.ToInt32(str);
+            return number;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка!!!!! Неверное число. Попробуйте еще раз только цифры.");
+            Console.WriteLine();
+        }
+    }
+}
+void Write(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(String.Join(", ", arr[i]));
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите минимум: ");
+int min = Check();
+Console.WriteLine("Введите Max: ");
+int max = Check();
+int[] myarray = CreateArray(min, max);
+Write(myarray);
+int result = NaturalisSum(myarray);
+Console.WriteLine();
+Console.WriteLine($"Cумма  натуральных элементов в промежутке от {min}  до {max} = {result}");
---
+++ Seminar_08/Task68/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+// функция Аккермана
+int AckermannMethod(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return AckermannMethod(n - 1, 1);
+    else
+        return AckermannMethod(n - 1, AckermannMethod(n, m - 1));
+}
+// Функция получение строки и проверка на число > 0.
+int CheckNumberMoreZero()
+{
+    int number;
+    while (true)
+    {
+        Console.WriteLine($"Введите число не менее 0: ");
+        string str = Console.ReadLine();
+        try
+        {
+            number = Convert.ToInt32(str);
+            while (number >= 0)
+            {
+                try
+                {
+                    return number;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка!!!!! Неверное число. необходимо >0.");
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка!!!!! Неверное число. Попробуйте еще раз только цифры.");
+            Console.WriteLine();
+        }
+    }
+}
+Console.WriteLine("Введите число n для функции Аккермана: ");
+int n = CheckNumberMoreZero();
+Console.WriteLine("Введите число m для функции Аккермана: ");
+int m = CheckNumberMoreZero();
+int result = AckermannMethod(n, m);
+Console.WriteLine();
+Console.WriteLine($"A(n,m) = {result}");
