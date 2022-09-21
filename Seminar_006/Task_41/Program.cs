ec6ce228cf5aab19810038e6199e6f233e8ae226
serg <svgordienko@yandex.ru>
2022-09-21 19:15:51 +0400

    add seminar06

---
+++ Seminar_006/Task_41/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+//Функция получение строки и проверка на число > 0.
+int CheckNumberMoreZero()
+{
+    int number;
+    while (true)
+    {
+        Console.WriteLine($"Введите число не менее 1: ");
+        string str = Console.ReadLine();
+        try
+        {
+            number = Convert.ToInt32(str);
+            if (number >= 1)
+            {
+                return number;
+            }
+            Console.WriteLine("Ошибка!!!!! Неверное число. необходимо >0.");
+            Console.WriteLine();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка!!!!! Неверное число. Попробуйте еще раз только цифры.");
+            Console.WriteLine();
+        }
+    }
+}
+int[] GetArray(int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        int num = 0;
+        while (true)
+        {
+            Console.WriteLine($"Введите число: ");
+            string str = Console.ReadLine();
+            try
+            {
+                num = Convert.ToInt32(str);
+                break;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка!!!!! Неверное число. Попробуйте еще раз только цифры.");
+                Console.WriteLine();
+            }
+        }
+        array[i] = num;
+    }
+    return array;
+}
+//функция вывода массива
+void Write(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(String.Join(", ", arr[i]));
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+void CountPositivNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Вы ввели {count} чисел больше 0.");
+}
+Console.WriteLine("Введите сколько Вы хотите ввести чисел: ");
+int countNum = CheckNumberMoreZero();
+int[] myarray = GetArray(countNum);
+Console.WriteLine();
+Write(myarray);
+Console.WriteLine();
+CountPositivNum(myarray);
---
+++ Seminar_006/Task_43/Program.cs
@@ -0,0 +1,64 @@
+﻿
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+double[] CrossPoint(double k1, double b1, double k2, double b2)// возвращает массив [o] наличие пересечения
+{
+    double[] point = new double[3];
+    if (k1 == k2)
+    {
+        point[0] = 0;
+        return point;
+    }
+    point[0] = 1;
+    point[1] = (b2 - b1) / (k1 - k2);
+    point[2] = k1 * point[1] + b1;
+    return point;
+}
+void Write(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(String.Join(", ", arr[i]));
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+// Функция получение строки и проверка на число double
+double CheckDouble()
+{
+    double number;
+    while (true)
+    {
+        Console.WriteLine($": ");
+        string str = Console.ReadLine();
+        try
+        {
+            number = Convert.ToDouble(str);
+            return number;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка!!!!! Неверное число. Попробуйте еще раз только цифры.");
+            Console.WriteLine();
+        }
+    }
+}
+Console.WriteLine("Введите коэффицент к1");
+double k1 = CheckDouble();
+Console.WriteLine("Введите коэффицент b1");
+double b1 = CheckDouble();
+Console.WriteLine("Введите коэффицент к2");
+double k2 = CheckDouble();
+Console.WriteLine("Введите коэффицент b2");
+double b2 = CheckDouble();
+double[] pointarr = CrossPoint(k1, b1, k2, b2);
+if (pointarr[0]==0)
+{
+Console.WriteLine("Прямые не пересекаются или линии одинаковы");
+}
+else
+{
+Console.WriteLine($"Координаты точки пересечения прямых: {pointarr[1]},{pointarr[2]}");
+}
